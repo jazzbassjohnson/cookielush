36853b4285c422c1f954824c47d8fcf0f2d21090
Gary Johnson <jazzbassjohnson@gmail.com>
2014-05-24 21:09:44 -0700

    Mock-up for CSS Animation is in Effect!

--- js/cookielush.js
+++ js/cookielush.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	  $('.img-holder').imageScroll();
+	  $('#cookieImage').on("click", function(){
+	  	$('#cookieImage').animate({width: "25px",
+	  		height: "25px",
+	  		top:"0px",
+	  		left: "0px",
+	  		margin: "20px",
+	  		position: "none",
+	  	}, 2000);
+	  	$('#cookieImage p').hide();
+
+	  });
 });
